985ad29e38e9f980c8936560a8e10b9193f30b26
Leandro-Pinho <leandroessencia@gmail.com>
2023-08-23 10:50:14 -0300

    create routes and pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Home from './pages/home/Home';
+import About from './pages/about/About';
+import Portfolio from './pages/portfolio/Portfolio';
+import Contact from './pages/contact/Contact';
+import Navbar from './components/Navbar';
+
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      hello, world!
-    </>
-  )
+function App() {
+  return <BrowserRouter>
+    <Routes>
+      <Navbar />
+      <Route index element={<Home />} />
+      <Route path='about' element={<About />} />
+      <Route path='portfolio' element={<Portfolio />} />
+      <Route path='contact' element={<Contact />} />
+    </Routes>
+  </BrowserRouter>
 }
 
 export default App
