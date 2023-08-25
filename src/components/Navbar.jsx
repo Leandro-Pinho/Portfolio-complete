1e38e8c2941fcccf6bb5dc359834c6fb08028d22
Leandro-Pinho <leandroessencia@gmail.com>
2023-08-25 11:23:11 -0300

    create navbar e estilizando, usando algumas animações

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import { links } from '../data';
+import './navbar.css';
+
 
 const Navbar = () => {
+    const [showMenu, setShowMenu] = useState(false)
+
     return (
-        <div>
-            navbar
-        </div>
+        <nav className="nav">
+            <div className={`${showMenu ? "nav__menu show-menu" : "nav__menu"}`}>
+                <ul className="nav__list">
+                    {links.map(({ name, icon, path, id }) => {
+                        return (
+                            <li className="nav__item" key={id}>
+                                <NavLink
+                                    to={path}
+                                    className={({ isActive }) =>
+                                        isActive ? 'nav__link active-nav' : 'nav__link'
+                                    }
+                                    onClick={() => setShowMenu(!showMenu)}
+                                >
+                                    {icon}
+                                    <h3 className="nav__name">{name}</h3>
+                                </NavLink>
+                            </li>
+                        )
+                    })}
+                </ul>
+            </div>
+
+            <div className={`${showMenu ? "nav__toggle animate-toggle" : "nav__toggle"}`} onClick={() => setShowMenu(!showMenu)}>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </nav>
     )
 }
 
